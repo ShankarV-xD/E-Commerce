0cefeb2f303f07eabe37d2fe1fbb2c7203779dd0
ShankarV-xD <shankarv1012@gmail.com>
2023-12-18 22:42:08 +0530

    post to get in route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const { loginCheck, isAuth, isAdmin } = require("../middleware/auth");
 
 router.post("/isadmin", authController.isAdmin);
 router.post("/signup", authController.postSignup);
-router.post("/signin", authController.postSignin);
+router.get("/signin", authController.postSignin);
 router.post("/user", loginCheck, isAuth, isAdmin, authController.allUser);
 
 module.exports = router;
